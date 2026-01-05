39c88951a16599d5d56911425d5e43924d4314ca
Andre Souza <asouza1807@gmail.com>
2026-01-05 17:13:34 -0300

    configuracao do app/core/config.py para conexao com o DB

--- MKT-LOGISTIC-ON-DEMAND/backend/app/core/config.py
+++ MKT-LOGISTIC-ON-DEMAND/backend/app/core/config.py
@@ -2,17 +2,15 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    # Definimos valores padrão aqui.
-    # Assim, se o .env falhar, o app ainda sobe com SQLite.
-    DATABASE_URL: str = "sqlite:///./database.db"
-    DATABSE_URL: str = "sqlite:///./database.db"  # Adicionei os dois por segurança
-    SECRET_KEY: str = "chave_temporaria_123"
 
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        env_file_encoding="utf-8",
-        extra="ignore"  # Isso evita erros se houver outras coisas no .env
-    )
+    DATABSE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACESS_TOKEN_EXPIRE_MINUTES: int = 60
+
+
+class Config:
+    env_file = ".env"
 
 
 settings = Settings()

3212227501b0b54b08a6df806de44940e4039bfa
Andre Souza <asouza1807@gmail.com>
2026-01-05 16:27:37 -0300

    API 200 ok - Funcionando corretamente

--- MKT-LOGISTIC-ON-DEMAND/backend/app/main.py
+++ MKT-LOGISTIC-ON-DEMAND/backend/app/main.py
@@ -1,22 +1,14 @@
-from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from app.api.auth import router as auth_router
 from app.api.loads import router as loads_router
-from app.core.database import init_db  # Importe sua função aqui
 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # O código aqui roda quando o app liga
-    init_db()
-    yield
-    # O código aqui roda quando o app desliga
-
-app = FastAPI(
-    title="Plataforma Privada de Cargas",
-    lifespan=lifespan
-)
+app = FastAPI(title="Plataforma Privada de Cargas")
 
 app.include_router(auth_router)
 app.include_router(loads_router)
+
+
+@app.get("/")
+def healthcheck():
+    return {"status": "ok"}

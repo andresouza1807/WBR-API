8d82220da22b58d64016767703d2dc759cdae636
Andre Souza <asouza1807@gmail.com>
2026-01-05 15:23:29 -0300

    configuracao api/loads.py

--- MKT-LOGISTIC-ON-DEMAND/backend/app/api/loads.py
+++ MKT-LOGISTIC-ON-DEMAND/backend/app/api/loads.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException
-from sqlmodel import Session
+from sqlmodel import Session, select
+
 from app.models.load import Load
 from app.core.database import get_session
+from app.models.user import User
+from app.api.deps import get_current_user
+
 router = APIRouter(prefix="/loads", tags=["Loads"])
 
 
@@ -11,3 +15,13 @@ def create_load(load: Load, session: Session = Depends(get_session)):
     session.commit()
     session.refresh(load)
     return load
+
+
+@router.get("/{current_user}")
+def list_loads(
+        current_user: User = Depends(get_current_user),
+        session: Session = Depends(get_session)):
+
+    return session.exec(
+        select(Load).where(Load.company_id == current_user.company_id)
+    ).all()
